d6b89839df5108e3c1b07b60f4b1a5592499c151
Ainur <sibrinat616@gmail.com>
2023-11-08 10:41:44 +0300

    just new design

--- productAppSwift/productAppSwift/ViewController.swift
+++ productAppSwift/productAppSwift/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextView1: UITextView!
+    @IBOutlet weak var TextView2: UITextView!
+    @IBOutlet weak var TextView3: UITextView!
+    @IBOutlet weak var TextView4: UITextView!
+    @IBOutlet weak var TextView5: UITextView!
+    
+    @IBAction func testButton(_ sender: Any) {
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
+    
 
-
+    
 }
 
